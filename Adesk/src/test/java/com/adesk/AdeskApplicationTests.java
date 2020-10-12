61c95d0d146fea6b55e026b5a413847f53f7812f
bernardlcs <bernard.lenin4@gmail.com>
2020-10-12 15:08:42 -0300

    Commit inicial desse projeto que sera uma beleeeeeeeza

---
+++ Adesk/src/test/java/com/adesk/AdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
