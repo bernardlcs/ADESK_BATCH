dfd6a67060dc12568c904979a15483a245990f6c
bernardlcs <bernard.lenin4@gmail.com>
2020-10-12 17:27:10 -0300

    adição de assinatura de banco de dadosnas classes, configuração banco oracle sql

--- Adesk/src/main/java/com/adesk/component/Aluno.java
+++ Adesk/src/main/java/com/adesk/component/Aluno.java
@@ -1,5 +1,101 @@
 package com.adesk.component;
 
+import java.util.Date;
+
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * @author Bernard/Patricia
+ *
+ */
+
+@Entity
+@Table (name="ALUNO")
 public class Aluno {
+	
+	@Id	
+	@SequenceGenerator(name = "ALUNO_SEQ", sequenceName = "ALUNO_SEQ",  allocationSize = 1) // Ambiente
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ALUNO_SEQ")
+	private int id;
+
+	@Column(name="NOME")
+	private String nome;
+	@Column(name="MAE")
+	private String mae;
+	@Column(name="PAI")
+	private String pai;
+	@Column(name="IDADE")
+	private int idade;
+	@Column(name="MODALIDADE")
+	private String modalidade;
+	@Column(name="DATA_MENSALIDADE")
+	private Date dataMensalidade;
+	@Column(name="MENSALIDADE_EM_DIA")
+	private String mensalidadeEmDia="OK";
+	
+	
+	
+	public Aluno() {
+		
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getMae() {
+		return mae;
+	}
+
+
+	public void setMae(String mae) {
+		this.mae = mae;
+	}
+
+
+	public String getPai() {
+		return pai;
+	}
+
+
+	public void setPai(String pai) {
+		this.pai = pai;
+	}
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+
+	public String getModalidade() {
+		return modalidade;
+	}
+
+
+	public void setModalidade(String modalidade) {
+		this.modalidade = modalidade;
+	}
+	
 
 }
--- Adesk/src/main/java/com/adesk/component/Colaborador.java
+++ Adesk/src/main/java/com/adesk/component/Colaborador.java
@@ -1,5 +1,61 @@
 package com.adesk.component;
 
-public class Colaborador {
+import java.util.ArrayList;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * @author Bernard/Patricia
+ *
+ */
+@Entity
+@Table (name="COLABORADOR")
+public class Colaborador {
+	@Id	
+	@SequenceGenerator(name = "COLABORADOR_SEQ", sequenceName = "COLABORADOR_SEQ",  allocationSize = 1) // Ambiente
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "COLABORADOR_SEQ")
+	private int id;
+
+	@Column(name="NOME")
+	private String nome;
+	@Column(name="IDADE")
+	private int idade;
+	@Column(name="FUNCAO")
+	private String funcao;
+	
+	public Colaborador() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+
+	
 }
